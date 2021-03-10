c80fffbfd132bdefba2163d772d02e2c1a93d475
Kristianto Haryodi <34808653+KrisHaryodi@users.noreply.github.com>
2021-03-10 09:41:17 +0700

    update

---
+++ protoMarketHub.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="UTF-8">
+        <title>MarketHub</title>
+        <link rel="stylesheet" href="styles/style.css">
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="shortcut icon" href="assets/image 10.png" type="image/x-icon">
+        <link href="https://fonts.googleapis.com/css2?family=Assistant:wght@200;300;400;600;700;800&family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="http://code.jquery.com/jquery-latest.js"></script>
+    </head>
+    <body>
+        <!-- Navbar -->
+        <div class="container">
+            <nav class="row navbar">
+                <div class="col-3 navbar-collapse">
+                <p style="font-weight: 700; font-size: 20px; color: #335849; padding-top: 5px; margin:10px;"><img style="margin-bottom: -10px; height:33px;" src="assets/image 10.png"> SiBakul Jogja</p>
+                    <ul class="navbar-nav ml-auto mr-3">
+                        <li class="nav-item mx-md-2">
+                            <a href="#" class="nav-link">Bela SiBakul</a>
+                        </li>
+                        <li class="nav-item mx-md-2">
+                            <a href="protoMarketHub.php" class="nav-link ac">MarketHub</a>
+                        </li>
+                        <li class="nav-item mx-md-2">
+                            <a href="#" class="nav-link">UMKM</a>
+                        </li>
+                        <li class="nav-item mx-md-2">
+                            <a href="#" class="nav-link">Koperasi</a>
+                        </li>
+                        <li class="nav-item mx-md-2">
+                            <a href="#" class="nav-link">Beranda</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </body>
+</html>
